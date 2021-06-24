f1819dbd9b1d09d29431eb77b47e98bacff1e5f4
patrick ojunde <patrickojunde@gmail.com>
2021-06-24 04:38:50 +0100

    updates

--- src/main/java/com/cova/quizapp/serviceimpl/TriviaHistoryServiceIMpl.java
+++ src/main/java/com/cova/quizapp/serviceimpl/TriviaHistoryServiceIMpl.java
@@ -2,13 +2,15 @@ package com.cova.quizapp.serviceimpl;
 
 import com.cova.quizapp.data.TriviaHistoryRepository;
 import com.cova.quizapp.service.ITriviaHistoryService;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Data
 public class TriviaHistoryServiceIMpl implements ITriviaHistoryService {
 
-    private TriviaHistoryRepository triviaHistoryRepository;
+    private  TriviaHistoryRepository triviaHistoryRepository;
 
     @Autowired
     public TriviaHistoryServiceIMpl(TriviaHistoryRepository triviaHistoryRepository){
--- src/main/java/com/cova/quizapp/serviceimpl/TriviaServiceImpl.java
+++ src/main/java/com/cova/quizapp/serviceimpl/TriviaServiceImpl.java
@@ -2,10 +2,12 @@ package com.cova.quizapp.serviceimpl;
 
 import com.cova.quizapp.data.TriviaRepository;
 import com.cova.quizapp.service.ITriviaService;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Data
 public class TriviaServiceImpl implements ITriviaService {
 
     private TriviaRepository triviaRepository;;
