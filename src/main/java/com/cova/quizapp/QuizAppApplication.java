e22014fd963f4147d23a5a66d68d938ec34c8e69
patrick ojunde <patrickojunde@gmail.com>
2021-06-26 05:55:36 +0100

    excluded spring default SecurityAutoConfiguration.class

--- src/main/java/com/cova/quizapp/QuizAppApplication.java
+++ src/main/java/com/cova/quizapp/QuizAppApplication.java
@@ -3,11 +3,12 @@ package com.cova.quizapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.cova.quizapp.model.persistence.repositories")
 @EntityScan("com.cova.quizapp.model.persistence")
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class QuizAppApplication {
 
     public static void main(String[] args) {
