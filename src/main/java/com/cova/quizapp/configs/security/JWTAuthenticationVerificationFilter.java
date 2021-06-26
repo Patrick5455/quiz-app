e3bd2a9cb1c1fabff0c81b7e5ed314262ea5a874
patrick ojunde <patrickojunde@gmail.com>
2021-06-26 09:38:44 +0100

    updates on security

--- src/main/java/com/cova/quizapp/configs/security/JWTAuthenticationVerificationFilter.java
+++ src/main/java/com/cova/quizapp/configs/security/JWTAuthenticationVerificationFilter.java
@@ -1,16 +1,20 @@
 package com.cova.quizapp.configs.security;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
@@ -22,6 +26,21 @@ public class JWTAuthenticationVerificationFilter extends BasicAuthenticationFilt
     @Autowired
     public JWTAuthenticationVerificationFilter(AuthenticationManager authMnger){
         super(authMnger);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)  throws IOException, ServletException {
+
+        String header = req.getHeader(AUTHORIZATION_HEADER);
+
+        if(header == null || !header.startsWith(TOKEN_PREFIX)){
+            chain.doFilter(req, res);
+            return;
+        }
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+
     }
 
     private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req){
